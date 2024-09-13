2878bd07f684a522f969129e93369815e22bbe1f
djones134 <djones134@ggc.edu>
2024-09-13 16:42:04 -0400

    UPDATED

--- src/Bed.java
+++ src/Bed.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+
 
 public class Bed {
+    private String name;
+    private String style;
+    private String size;
+
+    public Bed(){
+        this("my Bedroom", "modern", "large");
+
+    }
+    public Bed(String name, String style, String size){
+        this.name = name;
+        this.style = style;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public String getSize() {
+        return size;
+    }
+    public void make(){
+        System.out.println("From Bed class: make() method");
+    }
 }
--- src/Bedroom.java
+++ src/Bedroom.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
-
 public class Bedroom {
+    String name;
+    Wall w1;
+    Wall w2;
+    Wall w3;
+    Wall w4;
+
+    Ceiling ceil;
+
+    Bed bed;
+    Lamp lamp;
+
+    public Bedroom() {
+
+    }
+    public Bedroom(String name, Wall w1, Wall w2, Wall w3, Wall w4, Ceiling ceiling){
+        this.name = name;
+        this.w1 = w1;
+        this.w2 = w2;
+        this.w3 = w3;
+        this.w4 = w4;
+
+
+        this.ceil = ceiling;
+    }
+    public String getName(){
+        return name;
+    }
+    public  Wall getW1(){
+        return w1;
+    }
+    public void makeBed(){
+        System.out.println("Bedroom class --> calling makeBed() method");
+
+        //call make() method you declared in the Bedroom class
+        this.make();
+
+        //call make() method created
+        bed.make();
+    }
+    public void make(){
+        System.out.println("From Bedroom class - make() method");
+    }
+
+
+
+
 }
